d5c3842aae0e0bdf180223dfa0767c59236a9125
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-04-18 01:40:57 +0300

    add index.js changed

--- src/index.js
+++ src/index.js
@@ -12,18 +12,12 @@ const pageSize = 40;
 
 let windowHeight = document.documentElement.clientHeight;
 
-//? const refs = {
-//     form: document.querySelector('#search-form'),
-//     loadMoreBtn: document.querySelector('.load-more'),
-// }
+
 const formEl = document.querySelector('#search-form');
 const loadMoreBtnEl = document.querySelector('.load-more');
 loadMoreBtnEl.classList.add('is-hidden');
 
-//? const onBtnClick = (event) => {
-//     event.preventDefault();
-//     console.log(event.currentTarget.searchQuery.value);
-// }
+/
 formEl.addEventListener('submit', onSearchFormSubmit);
 function onSearchFormSubmit(event) {
   event.preventDefault();
@@ -37,12 +31,12 @@ function onSearchFormSubmit(event) {
   if (searchQuery) {
     fetchImages(searchQuery, page)
       .then(recievedHits => {
-        //? const hitsAmount = recievedHits.data.totalHits;  (axios/fetch)
+        
         const hitsAmount = recievedHits.totalHits;
 
         if (hitsAmount !== 0) {
           Notiflix.Notify.success(`Hooray! We found ${hitsAmount} images.`);
-          //? renderGallery(recievedHits.data.hits);  (axios/fetch)
+          
           renderGallery(recievedHits.hits);
 
           if (hitsAmount > pageSize) {
@@ -56,7 +50,7 @@ function onSearchFormSubmit(event) {
       })
       .catch(error => {
         console.log(error);
-        // Notiflix.Notify.failure('Oops, there is no country with that name');
+        
       });
   }
   page += 1;
@@ -67,36 +61,14 @@ function onSearchFormSubmit(event) {
 const MY_KEY = '26745683-ec5771c1b459334d80ff30e1d';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
-// ? VERSION 1 with async/await foo
+
 async function fetchImages(searchTerm, page) {
   const { data } = await axios.get(
     `/?key=${MY_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${pageSize}`,
   );
   return data;
 }
-// ? VERSION 2 with axios libs (fetch)
-// function fetchImages(searchTerm, page) {
-//   return axios.get(
-//     `/?key=${MY_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${pageSize}`,
-//   );
-// }
-// * VERSION 2 with axios libs
-// axios.defaults.baseURL = 'https://restcountries.com/v3.1';
-// axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
-// function fetchCountries(countryName) {
-//   return axios.get(`/name/${countryName}?fields=name,capital,languages`);
-// }
-// * VERSION 3 simle with fetch
-// const BASE_URL = 'https://restcountries.com/v3.1';
-// function fetchCountries(countryName) {
-//   const url = `${BASE_URL}/name/${countryName}?fields=name,capital,population,flags,languages`;
-//   return fetch(url).then(response => {
-//     if (!response.ok) {
-//       throw new Error(`Error: ${response.statusText}`);
-//     }
-//     return response.json();
-//   });
-// }
+
 
 const galleryEl = document.querySelector('.gallery');
 function renderGallery(cards) {
